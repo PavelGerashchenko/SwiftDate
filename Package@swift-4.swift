31e39321801e17cee8634c4d599d9c5cbf50e1f7
Pavel Gerashchenko <p.gerashchenko@gmail.com>
2018-01-21 18:16:01 +0300

    Update Package@swift-4.swift

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -3,5 +3,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftDate"
+    name: "SwiftDate",
+    products: [
+        .library(name: "SwiftDate", type: .dynamic, targets: ["SwiftDate"])],
+    targets:[
+        .target(name: "SwiftDate")
+    ]
 )
